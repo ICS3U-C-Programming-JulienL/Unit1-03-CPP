abfc792b6ed9007a28f87c6347ee8b64c57ed35a
Julien Lamoureux <julien.lamoureux@stu.ocsb.ca>
2023-09-16 18:49:17 +0000

    Added helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Julien Lamoureux All rights reserved.
+// .
+// Created by: Julien Lamoureux
+// Date: September 16, 2023
+// This program displays "Hello, World with style".
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with style!" << std::endl;
+    std::cout << "C++ is a great coding language!" << std::endl;
+}
